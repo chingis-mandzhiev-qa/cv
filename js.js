e22da36a2b069c75d3394fc614e2097643e2f38f
chingis-mandzhiev-qa <166165772+chingis-mandzhiev-qa@users.noreply.github.com>
2024-04-05 21:48:07 +0300

    Update js.js

--- js.js
+++ js.js
@@ -4,5 +4,5 @@ var button = document.querySelector('button');
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
 	console.log('log: Выведи этот текст в консоль если кнопка нажмётся');
-	alert('"От себя" и "На себя"')
+	alert('"Деньги это не главное"')
 })
